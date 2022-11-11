a9ab5bcc4798fa8f213db595a62afffb1de835d0
Victor Salles <victor.salles@engenharia.ufjf.br>
2022-11-11 18:22:32 -0300

    feat: Req 8 - Fix: Req 6 (refatorado)

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,7 @@
 export const CREATE_USER = 'CREATE_USER';
 export const SAVE_IMAGE = 'SAVE_IMAGE';
+export const TIMER_OUT_TRUE = 'TIMER_OUT_TRUE';
+export const TIMER_OUT_FALSE = 'TIMER_OUT_FALSE';
 export const createUser = (payload) => ({
   type: CREATE_USER,
   payload,
@@ -7,4 +9,11 @@ export const createUser = (payload) => ({
 export const saveImage = (payload) => ({
   type: SAVE_IMAGE,
   payload,
+});
+export const timerOutTrue = () => ({
+  type: TIMER_OUT_TRUE,
+});
+
+export const timerOutFalse = () => ({
+  type: TIMER_OUT_FALSE,
 });
