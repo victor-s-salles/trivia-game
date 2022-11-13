e9234c5c40ef15052f0c503d1c16f4a0dd539967
othiagomanhaes <othiagomanhaes@gmail.com>
2022-11-13 19:02:38 -0300

    Feat: botão que faz jogar novamente.

---
+++ src/components/BtnPlayAgain.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class BtnPlayAgain extends Component {
+  goToStart = () => {
+    const { history } = this.props;
+    history.push('/game');
+  };
+
+  render() {
+    return (
+      <button
+        type="button"
+        data-testid="btn-play-again"
+        onClick={ this.goToStart }
+      >
+        Play Again
+      </button>
+    );
+  }
+}
+
+BtnPlayAgain.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default BtnPlayAgain;
