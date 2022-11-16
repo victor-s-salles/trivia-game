96e8b695c7a3f74d7c1a836d2aad943e0116faf0
lucas-de-lima <lucasramosdelima@gmail.com>
2022-11-16 16:38:29 -0300

    fix: recriando arquivo Ranking.test.js

---
+++ src/utils/Ranking.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('Realiza testes na página Ranking', () => {
+  it('Verifica se existe a imagem do jogador', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe o nome do jogador', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe um campo com o score do jogador', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe a menssagem de feedback', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe a menssagem com o número de questões corretas', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe a menssagem com o total de pontos do jogador', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe o botão \'Play Again\'', () => {
+    fail('Teste vazio!');
+  });
+
+  it('Verifica se existe o botão \'Ver e Salvar Ranking\'', () => {
+    fail('Teste vazio!');
+  });
+});
